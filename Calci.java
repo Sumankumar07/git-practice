aa219246009eb2313b64eaae91b3589b348b716a
Sumankumar07 <sumankumar.ece2019@dscet.ac.in>
2024-03-01 01:24:01 +0530

    hello

---
+++ Calci.java
@@ -0,0 +1,17 @@
+interface Calci {
+
+    public int getSum(int n1, int n2);
+
+}
+
+class CalciImpl implements Calci {
+    public int getSum(int n1, int n2) {
+        return n1 + n2;
+    }
+}
+
+public class Driver {
+    public static void main(String[] args) {
+
+    }
+}
