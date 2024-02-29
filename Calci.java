b89efa511f816268b0e19a6754fb1441f293d5ca
Sumankumar07 <sumankumar.ece2019@dscet.ac.in>
2024-03-01 01:29:10 +0530

    modified

--- Calci.java
+++ Calci.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 interface Calci {
 
     public int getSum(int n1, int n2);
@@ -12,6 +14,11 @@ class CalciImpl implements Calci {
 
 public class Driver {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 2 number to get sum");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        CalciImpl c = new CalciImpl();
+        System.out.println(num1 + "+" + num2 + "=" + c.getSum(num1, num2));
     }
 }
